38ec31e360cd1d536f2e1d86c167c811ad734319
Thiago Biancucci <thiago.biancucci@mi.unc.edu.ar>
2025-10-24 23:46:51 -0300

    Update UI styles and chart visuals, rename assets
    Refactored CSS for improved color scheme and layout, enhancing card, header, and table styles. Renamed logo image assets for clarity and updated references in Header.jsx. Enhanced MetricChart.jsx with custom line and active dot styling for better chart visualization. Added an empty Dockerfile for future containerization.

--- WebApp/webapp/src/components/Header.jsx
+++ WebApp/webapp/src/components/Header.jsx
@@ -3,7 +3,9 @@ export default function Header() {
     <div className="card">
       <div className="header">
         <div>
-          <img src="WebApp/webapp/src/assets/1_1_Isologotipo FCEFyN y UNC- original_Sin fondo-Con bajada.png" alt="Logo FCEFyN" />
+          <img src="src/assets/logoFacultadNoBG.png" alt="Logo FCEFyN" />
+        </div>
+        <div>
           <h1>FCEFyN — Lectura de Datos Ambientales mediante sensores comunicados por LoRa y WiFi
           </h1>
           <p>Temperatura • Humedad • Presión — Datos IoT</p>
--- WebApp/webapp/src/components/MetricChart.jsx
+++ WebApp/webapp/src/components/MetricChart.jsx
@@ -2,6 +2,7 @@ import {
   LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer,
 } from "recharts";
 
+
 export default function MetricChart({ data, xKey = "time", yKey, unit }) {
   return (
     <div style={{ width: "100%", height: 280 }}>
@@ -11,7 +12,20 @@ export default function MetricChart({ data, xKey = "time", yKey, unit }) {
           <XAxis dataKey={xKey} tick={{ fontSize: 12 }} />
           <YAxis tick={{ fontSize: 12 }} />
           <Tooltip formatter={(v) => `${v} ${unit ?? ""}`} />
-          <Line type="monotone" dataKey={yKey} dot={false} />
+          <Line
+            type="monotone"
+            dataKey={yKey}
+            stroke="#2d6363"                 // color de la línea
+            dot={{ r: 3, fill: "#2d6363" }}  // dots normales (si quieres mostrarlos)
+            activeDot={{
+                
+              r: 6,
+              fill: "#d38129",
+              stroke: "#1d4036",
+              strokeWidth: 2,
+              
+            }}
+          />
         </LineChart>
       </ResponsiveContainer>
     </div>
